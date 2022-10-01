30149f4935f3ad5398680212886ad13ed1ad4882
Prajjwal Arora <prajjwalarora@geekyants.com>
2022-10-01 20:06:47 +0530

    doc fix

--- src/components/Editor/EditorBox/EditorBox.js
+++ src/components/Editor/EditorBox/EditorBox.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { createEditor, Editor, Text, Transforms } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import EditorWidgets from "../EditorWidgets/EditorWidgets";
@@ -7,14 +9,10 @@ import Leaf from "./Leaf";
 
 const EditorBox = ({ socket }) => {
   const [editor] = useState(() => withReact(createEditor()));
-  const [initialValue] = useState([
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }],
-    },
-  ]);
+  const [initialValue, setInitialValue] = useState([]);
+  const [searchParams] = useSearchParams();
   const userId = useMemo(() => {
-    localStorage.getItem("token");
+    localStorage.getItem("userId");
   });
 
   useEffect(() => {
@@ -27,6 +25,28 @@ const EditorBox = ({ socket }) => {
       });
     }
   }, [socket, editor]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(
+        `http://10.1.105.126:8080/api/v1/doc/${searchParams.get("docId")}`,
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(JSON.parse(res.data.data.data));
+        editor.children = JSON.parse(res.data.data.data);
+        editor.onChange();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
@@ -110,72 +130,70 @@ const EditorBox = ({ socket }) => {
       />
       <div className="px-12">
         <div className="p-5 w-full border rounded-xl">
-          <Slate
-            editor={editor}
-            value={initialValue}
-            onChange={(value) => {
-              const isAstChange = editor.operations.some(
-                (op) => "set_selection" !== op.type
-              );
-              if (isAstChange) {
-                const content = JSON.stringify(value);
-                console.log(value);
-                localStorage.setItem("content", content);
-                socket.emit(
-                  "input-by-user",
-                  content,
-                  "6337e050de39fc7d9b8e008b"
+          {initialValue && (
+            <Slate
+              editor={editor}
+              value={initialValue}
+              onChange={(value) => {
+                const isAstChange = editor.operations.some(
+                  (op) => "set_selection" !== op.type
                 );
-              }
-            }}
-          >
-            <Editable
-              renderElement={renderElement}
-              renderLeaf={renderLeaf}
-              onKeyDown={(event) => {
-                if (!event.ctrlKey) {
-                  return;
-                }
-
-                switch (event.key) {
-                  case "`": {
-                    event.preventDefault();
-                    const [match] = Editor.nodes(editor, {
-                      match: (n) => n.type === "code",
-                    });
-                    Transforms.setNodes(
-                      editor,
-                      { type: match ? "paragraph" : "code" },
-                      { match: (n) => Editor.isBlock(editor, n) }
-                    );
-                    break;
-                  }
-
-                  case "b": {
-                    event.preventDefault();
-                    Transforms.setNodes(
-                      editor,
-                      { bold: true },
-
-                      { match: (n) => Text.isText(n), split: true }
-                    );
-                    break;
-                  }
-
-                  case "i": {
-                    event.preventDefault();
-                    Transforms.setNodes(
-                      editor,
-                      { italic: true },
-
-                      { match: (n) => Text.isText(n), split: true }
-                    );
-                    break;
-                  }
+                if (isAstChange) {
+                  const content = JSON.stringify(value);
+                  console.log(value);
+                  localStorage.setItem("content", content);
+                  socket.emit("input-by-user", content, userId);
                 }
               }}
-            />
-          </Slate>
+            >
+              <Editable
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+                onKeyDown={(event) => {
+                  if (!event.ctrlKey) {
+                    return;
+                  }
+
+                  switch (event.key) {
+                    case "`": {
+                      event.preventDefault();
+                      const [match] = Editor.nodes(editor, {
+                        match: (n) => n.type === "code",
+                      });
+                      Transforms.setNodes(
+                        editor,
+                        { type: match ? "paragraph" : "code" },
+                        { match: (n) => Editor.isBlock(editor, n) }
+                      );
+                      break;
+                    }
+
+                    case "b": {
+                      event.preventDefault();
+                      Transforms.setNodes(
+                        editor,
+                        { bold: true },
+
+                        { match: (n) => Text.isText(n), split: true }
+                      );
+                      break;
+                    }
+
+                    case "i": {
+                      event.preventDefault();
+                      Transforms.setNodes(
+                        editor,
+                        { italic: true },
+
+                        { match: (n) => Text.isText(n), split: true }
+                      );
+                      break;
+                    }
+                  }
+                }}
+              />
+            </Slate>
+          )}
         </div>
       </div>
     </>
